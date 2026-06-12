12da518fa9335f84a4ccfecfd27c57d8dff5958f
PepperPixels <pepperypixels@gmail.com>
2026-06-12 18:14:53 +0200

    Update bad items list in BadItemsEquipped.lua
    Added additional bad items to the equipped item check.

--- BadItemsEquipped.lua
+++ BadItemsEquipped.lua
@@ -2,38 +2,46 @@ function()
     local badItems = { -- Item list: [itemId from wowhead] = "<name>",
         -- Black Temple teleport neck
         [32757] = "Blessed Medallion of Karabor",
-
+        
         -- Timeless Isle teleport trinket
         [103678] = "Time-Lost Artifact",
-
+        
         -- Fishing pole
         [19970] = "Arcanite Fishing Pole",
-
+        
         -- Guild teleport cloaks
         [63352] = "Shroud of Cooperation",
         [63207] = "Shroud of Cooperation",
         [63206] = "Wrap of Unity",
         [65360] = "Cloak of Coordination",
         [65274] = "Cloak of Coordination",
-
+        
         -- Dalaran teleport rings
         [40585] = "Signet of the Kirin Tor",
         [51557] = "Runed Signet of the Kirin Tor",
         [45691] = "Inscribed Signet of the Kirin Tor",
+        
+        -- Fishing and other stuff
+        [50287] = "Boots of the Bay",
+        [19979] = "Hook of the Master Angler",
+        [19972] = "Lucky Fishing Hat",
+        [33820] = "Weather-Beaten Fishing Hat",
+        [86566] = "Forager's Gloves",
+        
     }
-
+    
     aura_env.itemName = nil
-
+    
     local _, instanceType = GetInstanceInfo()
     local isGrouped = IsInGroup()
-
+    
     if instanceType ~= "raid" and instanceType ~= "party" and not isGrouped then
         return false
     end
-
+    
     for slot = 1, 19 do
         local itemID = GetInventoryItemID("player", slot)
-
+        
         if itemID and badItems[itemID] then
             aura_env.itemName = badItems[itemID]
             aura_env.itemID = itemID
@@ -41,6 +49,7 @@ function()
             return true
         end
     end
-
+    
     return false
 end
+
